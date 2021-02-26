fb7f7b4ce35801fb89f487210e03719de4d281c0
Nitish Malhotra <nitish.malhotra@gmail.com>
2021-02-25 16:02:44 -0800

    Finalizer update failure handling improvement (#79)
    Set the spec values to the passed in overlay values' value
    
    Signed-off-by: nitishm <nitishm@microsoft.com>
    
    Co-authored-by: nitishm <nitishm@microsoft.com>

--- controllers/appgroup_controller.go
+++ controllers/appgroup_controller.go
@@ -13,7 +13,6 @@ import (
 	"github.com/Azure/Orkestra/pkg/configurer"
 	"github.com/Azure/Orkestra/pkg/registry"
 	"github.com/Azure/Orkestra/pkg/workflow"
-	v1 "github.com/fluxcd/helm-operator/pkg/apis/helm.fluxcd.io/v1"
 	"github.com/go-logr/logr"
 	"helm.sh/helm/v3/pkg/chart"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -95,9 +94,11 @@ func (r *ApplicationGroupReconciler) Reconcile(req ctrl.Request) (ctrl.Result, e
 
 	v := orkestrav1alpha1.ApplicationGroup{}
 	for _, app := range appGroup.Spec.Applications {
-		app.Spec.Values = v1.HelmValues{
-			Data: make(map[string]interface{}),
+		if app.Spec.Overlays.Data == nil {
+			app.Spec.Overlays.Data = make(map[string]interface{})
 		}
+		app.Spec.Values = app.Spec.Overlays
+
 		v.Spec.Applications = append(v.Spec.Applications, app)
 	}
 
