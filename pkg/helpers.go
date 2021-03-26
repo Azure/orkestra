d00194a95d8d9056994d5bc674e2767a497fe0a2
Nitish Malhotra <nitishm@microsoft.com>
2021-03-26 15:06:58 -0700

    Limit length of names to be below 63 chars
    Signed-off-by: Nitish Malhotra <nitishm@microsoft.com>

---
+++ pkg/helpers.go
@@ -0,0 +1,17 @@
+package pkg
+
+import "strings"
+
+func ConvertToDNS1123(in string) string {
+	return strings.ReplaceAll(in, "_", "-")
+}
+
+func ToInitials(in string) (out string) {
+	in = ConvertToDNS1123(in)
+	parts := strings.Split(in, "-")
+
+	for _, part := range parts {
+		out += string(part[0])
+	}
+	return out
+}
