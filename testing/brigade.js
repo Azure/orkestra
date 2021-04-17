2ed21204301d95cc5b3cf8ff72c73bbd73dfb557
Dietrich <danaya@microsoft.com>
2021-04-17 05:11:14 -0700

    got the example project running with brigade

--- testing/brigade.js
+++ testing/brigade.js
@@ -1,29 +1,41 @@
 const { events, Job } = require('brigadier')
-const { KindJob } = require("@brigadecore/brigade-utils");
 
 events.on("exec", (brigadeEvent, project) => {
-  let kind = new KindJob("kind");
+  console.log("Running on exec")
+  let test = new Job("test-runner",)
+  
+  test.image = "ubuntu"
+  test.shell = "bash"
 
-  kind.tasks.push(
-    // add basic tools to image
-    "apk update",
-    "apk add --update --no-cache git",
-    "apk add --update --no-cache sudo",
-    "apk add --update --no-cache gcc",
-    // install Helm
+  test.tasks = [
+    "apt-get update -y",
+    "apt-get upgrade -y",
+    "apt-get install curl -y",
+    "apt-get install sudo -y",
+    "apt-get install git -y",
+    "apt-get install make -y",
+    "apt-get install wget -y",
+    "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.17/bin/linux/amd64/kubectl",
+    "chmod +x ./kubectl",
+    "sudo mv ./kubectl /usr/local/bin/kubectl",
+    "echo installed kubectl",
     "curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3",
     "chmod 700 get_helm.sh",
     "./get_helm.sh",
-    // clone Orkestra repo
+    "echo installed helm",
+    "wget -c https://golang.org/dl/go1.16.3.linux-amd64.tar.gz",
+    "tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz",
+    "export PATH=$PATH:/usr/local/go/bin",
+    "go version",
     "git clone https://github.com/Azure/Orkestra",
+    "echo cloned orkestra",
     "cd Orkestra",
     "make setup-kubebuilder",
     "kubectl apply -k ./config/crd",
-    "helm install orkestra chart/orkestra/  --namespace orkestra --create-namespace",
-    // apply example project
+    "helm install --wait orkestra chart/orkestra/ --namespace orkestra --create-namespace",
     "kubectl apply -f examples/simple/bookinfo.yaml",
-    "helm ls -A"
-  );
+    "kubectl get pods -A",
+  ]
 
-  return kind.run();
+  test.run()
 })
--- testing/helpfulnotes.md
+++ testing/helpfulnotes.md
@@ -1,6 +1,11 @@
 # Helpful Notes for writing tests with brigade
 brig v1 api
 
+In my testing I have found Brigade works best with Kind. Before you begin make sure docker and kind are running. 
+
+You will need to apply the binding.yml file to give the bridge-worker pods the ability to use kubectl commands. The Dockerfile will be the image the brigade job is run on. For now the brigade.js does the setup and grabs all the dependencies.
+
+
 To check logs of your Jobs use,
 
 ```
@@ -12,6 +17,17 @@ To install the brigadecore-utils at runtime add the --config flag to brig run wi
 
 ```
 
-brig run <Project> --file brigade.js --config brigade.json
+brig run <PROJECT_NAME> --file brigade.js --config brigade.json
  
 ```
+
+(Unnecessary since we are not using KindJob anymore) The KindJob object in the Brigade API requires you to allow mount hosts in the project. When creating your project with 
+
+```
+
+brig project create
+
+```
+
+Enter Y when asked for advanced options, this will allow you to set allow mount hosts to true.
+
