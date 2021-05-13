f00daa52e4e7e0ae44a8302b6bd87e66168c9698
Jonathan Innis <jonathan.innis.ji@gmail.com>
2021-05-13 00:22:57 -0700

    Fix create testing

--- controllers/appgroup_controller_test.go
+++ controllers/appgroup_controller_test.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"github.com/Azure/Orkestra/api/v1alpha1"
 	"github.com/Azure/Orkestra/pkg/meta"
+	v1alpha12 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	fluxhelmv2beta1 "github.com/fluxcd/helm-controller/api/v2beta1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -19,16 +22,24 @@ var _ = Describe("ApplicationGroup Controller", func() {
 	Context("ApplicationGroup", func() {
 		var (
 			namespace *corev1.Namespace
+			ctx       context.Context
+		)
+
+		const (
+			DefaultNamesapce = "orkestra"
 		)
 
 		BeforeEach(func() {
+			// TODO: Namespace will be added once we have the namespace based support for ApplicationGroup
 			namespace = &corev1.Namespace{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: "appgroup-test" + randStringRunes(5),
 				},
 			}
-			err := k8sClient.Create(ctx, namespace)
-			Expect(err).ToNot(HaveOccurred())
+			ctx = context.Background()
+			_ = k8sClient.Create(ctx, namespace)
+			//Expect(err).ToNot(HaveOccurred())
+
 		})
 
 		AfterEach(func() {
@@ -39,30 +50,43 @@ var _ = Describe("ApplicationGroup Controller", func() {
 		It("Should create Bookinfo spec successfully", func() {
 			ctx := context.Background()
 			applicationGroup := bookinfo()
-			applicationGroup.Namespace = namespace.Name
+			applicationGroup.Namespace = DefaultNamesapce
 			key := client.ObjectKeyFromObject(applicationGroup)
 
 			By("Applying the bookinfo object to the cluster")
-			Expect(k8sClient.Create(ctx, applicationGroup)).To(Succeed())
+			err := k8sClient.Create(ctx, applicationGroup)
+			Expect(err).ToNot(HaveOccurred())
 
 			// Defer the cleanup so that we delete the appGroup after creation
 			defer func() {
-				k8sClient.Delete(ctx, applicationGroup)
+				By("Deleting the bookinfo object from the cluster")
+				patch := client.MergeFrom(applicationGroup.DeepCopy())
+				controllerutil.RemoveFinalizer(applicationGroup, "application-group-finalizer")
+				_ = k8sClient.Patch(ctx, applicationGroup, patch)
+				_ = k8sClient.Delete(ctx, applicationGroup)
 			}()
 
 			helmReleaseList := &fluxhelmv2beta1.HelmReleaseList{}
-			err := k8sClient.List(ctx, helmReleaseList)
+			err = k8sClient.List(ctx, helmReleaseList)
 			Expect(err).ToNot(HaveOccurred())
 			oldHelmReleaseCount := len(helmReleaseList.Items)
 
+			By("Making sure that the workflow goes into a running state")
+			Eventually(func() bool {
+				workflow := &v1alpha12.Workflow{}
+				workflowKey := types.NamespacedName{Name: applicationGroup.Name, Namespace: applicationGroup.Namespace}
+				_ = k8sClient.Get(ctx, workflowKey, workflow)
+				return workflow.Status.Phase == v1alpha12.NodeRunning
+			}, time.Minute, time.Second).Should(BeTrue())
+
 			By("Waiting for the bookinfo object to reach a succeeded reason")
 			Eventually(func() bool {
-				temp := &v1alpha1.ApplicationGroup{}
-				if err := k8sClient.Get(ctx, key, temp); err != nil {
+				applicationGroup = &v1alpha1.ApplicationGroup{}
+				if err := k8sClient.Get(ctx, key, applicationGroup); err != nil {
 					return false
 				}
-				return temp.GetReadyCondition() == meta.SucceededReason
-			}, time.Minute*3, time.Second).Should(BeTrue())
+				return applicationGroup.GetReadyCondition() == meta.SucceededReason
+			}, time.Minute*4, time.Second).Should(BeTrue())
 
 			By("checking that the all the HelmReleases have come up and are in a ready state")
 			err = k8sClient.List(ctx, helmReleaseList)
@@ -75,6 +99,7 @@ var _ = Describe("ApplicationGroup Controller", func() {
 				}
 			}
 			Expect(allReady).To(BeTrue())
+
 		})
 	})
 })
