9fa876edcc39cf19757f075c5b642532238e8218
Davut Karabay <davutk@microsoft.com>
2021-05-24 10:49:32 -0700

    Rebased with the latest main

--- pkg/workflow/argo.go
+++ pkg/workflow/argo.go
@@ -19,6 +19,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
 
 const (
 	// The set of executor actions which can be performed on a helmrelease object
