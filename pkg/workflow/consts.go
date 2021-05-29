7c1ff10664dcdfb46a9aa431d14916f34833992c
Nitish Malhotra <nitish.malhotra@gmail.com>
2021-05-29 11:02:58 -0700

    Revert "Changes for using action parameter of orkestra workflow executor (#230)"
    This reverts commit 1344e26445008fd21aa69b3f75f517f019161a2f.

--- pkg/workflow/consts.go
+++ pkg/workflow/consts.go
@@ -15,7 +15,7 @@ const (
 
 	ExecutorName     = "executor"
 	ExecutorImage    = "azureorkestra/executor"
-	ExecutorImageTag = "v0.3.0"
+	ExecutorImageTag = "v0.2.0"
 
 	ChartMuseumName = "chartmuseum"
 )
