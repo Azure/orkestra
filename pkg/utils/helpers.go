0f2012f89515bcc1a184a496e1d4b2b2a2e07c76
Rajwinder Mahal <git@mahal.dev>
2021-06-08 21:05:56 -0700

    Generate a unique prefix instead of initials when naming subcharts (#283)
    * Generate unique prefix when naming subcharts
    
    * Limit max chart name len to 63 chars
    
    * Refactor code
    
    * Update pkg/utils/chart.go
    
    Co-authored-by: Nitish Malhotra <nitish.malhotra@gmail.com>
    
    Co-authored-by: Nitish Malhotra <nitish.malhotra@gmail.com>

--- pkg/utils/helpers.go
+++ pkg/utils/helpers.go
@@ -1,29 +1,36 @@
 package utils
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strings"
+
 	fluxhelmv2beta1 "github.com/fluxcd/helm-controller/api/v2beta1"
 	"sigs.k8s.io/yaml"
-	"strings"
 )
 
 func ConvertToDNS1123(in string) string {
 	return strings.ReplaceAll(in, "_", "-")
 }
 
-func ToInitials(in string) (out string) {
-	in = ConvertToDNS1123(in)
-	parts := strings.Split(in, "-")
-
-	for _, part := range parts {
-		out += string(part[0])
-	}
-	return out
-}
-
 func ConvertSliceToDNS1123(in []string) []string {
 	out := []string{}
 	for _, s := range in {
 		out = append(out, ConvertToDNS1123(s))
+	}
+	return out
+}
+
+func GetHash(in string) string {
+	h := sha256.New()
+	h.Write([]byte(in))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func TruncateString(in string, num int) string {
+	out := in
+	if len(in) > num {
+		out = in[0:num]
 	}
 	return out
 }
