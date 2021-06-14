48465f5419fa0c8cf25cd5bc1aa0f0681523a018
Rajwinder Mahal <git@mahal.dev>
2021-06-13 17:34:56 -0700

    Fix generated release name

--- pkg/utils/consts.go
+++ pkg/utils/consts.go
@@ -5,9 +5,6 @@ const (
 	DNS1123NotAllowedChars      = "[^-a-z0-9]"
 	DNS1123NotAllowedStartChars = "^[^a-z0-9]+"
 
-	// subchartNameMaxLen is the maximum length of a subchart name.
-	//
-	// The max name length limit enforced by DNS1123 is 63 chars. We reserve 10 chars
-	// for concatenating application name hash.
-	subchartNameMaxLen = 53
+	// hashedAppNameMaxLen is the maximum length of applicaiton name hash that is
+	hashedAppNameMaxLen = 10
 )
