2111de4fa937209cbbf17a2c6284c4de8cfc0f7a
Rajwinder Mahal <git@mahal.dev>
2021-06-17 13:33:03 -0700

    Refactor ConvertToDNS1123() (#335)

--- pkg/utils/consts.go
+++ pkg/utils/consts.go
@@ -1,10 +1,17 @@
 package utils
 
+import (
+	"regexp"
+)
+
 const (
-	DNS1123NameMaximumLength    = 63
-	DNS1123NotAllowedChars      = "[^-a-z0-9]"
-	DNS1123NotAllowedStartChars = "^[^a-z0-9]+"
+	DNS1123NameMaximumLength = 63
 
 	// hashedAppNameMaxLen is the maximum length of application name hash that is
 	hashedAppNameMaxLen = 10
+)
+
+var (
+	dns1123NotAllowedCharsRegexp      = regexp.MustCompile("[^-a-z0-9]")  // treat as const
+	dns1123NotAllowedStartCharsRegexp = regexp.MustCompile("^[^a-z0-9]+") // treat as const
 )
