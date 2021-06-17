8b06641dd77ddccdcc08078283013245601e772a
Rajwinder Mahal <git@mahal.dev>
2021-06-17 15:09:33 -0700

    bug fix: assignment to entry in nil map

--- api/v1alpha1/appgroup_types.go
+++ api/v1alpha1/appgroup_types.go
@@ -220,7 +220,7 @@ type ApplicationGroupStatus struct {
 // GetValues unmarshals the raw values to a map[string]interface{} and returns
 // the result.
 func (in *Application) GetValues() map[string]interface{} {
-	var values map[string]interface{}
+	values := make(map[string]interface{})
 	if in.Spec.Release.Values != nil {
 		_ = json.Unmarshal(in.Spec.Release.Values.Raw, &values)
 	}
